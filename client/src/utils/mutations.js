a86d77a8c9b4ba41e938102d0452c3f97bf2920d
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-13 13:52:16 -0500

    mutations for LOGIN_USER, ADD_USER, SAVE_BOOK, REMOVE_BOOK

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,3 +12,56 @@ export const LOGIN_USER = gql`
         }
     }
 `;
+
+export const ADD_USER = gql`
+
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+
+    mutation saveBook($input: bookInput!) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                title
+                description
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                title
+                description
+                link
+            }
+        }
+    }
+`;
